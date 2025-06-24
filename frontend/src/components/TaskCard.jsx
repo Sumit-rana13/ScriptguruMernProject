2ed2f197332d540ff1d3b7c359fca97a9e15b7da
Sumit Rana <118682671+Sumit-rana13@users.noreply.github.com>
2025-06-24 21:55:31 +0530

    modified

--- frontend/src/components/TaskCard.jsx
+++ frontend/src/components/TaskCard.jsx
@@ -1,11 +1,12 @@
 export default function TaskCard({ task, onStatusChange, onDelete }) {
   return (
-    <div className="bg-white rounded p-3 shadow mb-2  p-2 border border-gray-200 text-black">
+    <div className="bg-white rounded p-3 shadow mb-2 border border-gray-200 text-black">
       <h4 className="font-semibold">{task.title}</h4>
       <p className="text-sm text-gray-600">{task.description}</p>
       <p className="text-xs">Due: {task.dueDate?.slice(0, 10)}</p>
       <p className="text-xs">Assigned to: {task.assignedTo || 'Unassigned'}</p>
       {/* <p className="text-xs text-gray-500">Priority: {task.priority}</p> */}
+
       <span className={`text-xs px-2 py-1 rounded-full text-white ${
         task.priority === "High" ? "bg-red-500" :
         task.priority === "Medium" ? "bg-yellow-500" :
